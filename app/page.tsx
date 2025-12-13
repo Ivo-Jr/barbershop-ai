38ba4e5977b51586c40b3d8d64be59a1573f3e7e
ivojr <maciel.jr95@gmail.com>
2025-12-13 18:03:33 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,30 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import banner from "@/public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
       <div className="space-y-4 px-5">
         <SearchInput />
         <Image
           src={banner}
-          alt="Banner Aparatus"
+          alt="Banner Barber Shop AI"
           sizes="100vw"
           className="h-auto w-full"
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de Cabelo"
+          barberShopName="Barbearia do João"
+          barberShopImageUrl="https://utfs.io/f/7e309eaa-d722-465b-b8b6-76217404a3d3-16s.png"
+          date={new Date()}
+        />
       </div>
-    </div>
+    </main>
   );
 }
