fd822c87cb2078859d26d3d9d3bad77927b615c9
ivojr <maciel.jr95@gmail.com>
2025-12-31 16:26:45 -0300

    feat: validation of the session user when rendering schedules available for booking

--- app/_actions/get-date-available-time-slots.ts
+++ app/_actions/get-date-available-time-slots.ts
@@ -1,9 +1,11 @@
 "use server";
 
 import { actionClient } from "@/lib/action-client";
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { generateTimeSlots } from "@/lib/utils";
 import { endOfDay, format, startOfDay } from "date-fns";
+import { returnValidationErrors } from "next-safe-action";
 import { z } from "zod";
 
 const inputSchema = z.object({
@@ -14,6 +16,14 @@ const inputSchema = z.object({
 export const getDateAvailableTimeSlots = actionClient
   .inputSchema(inputSchema)
   .action(async ({ parsedInput: { barbershopId, date } }) => {
+    const session = await auth.api.getSession();
+
+    if (!session?.user) {
+      return returnValidationErrors(inputSchema, {
+        _errors: ["Unauthorized"],
+      });
+    }
+
     const bookings = await prisma.booking.findMany({
       where: {
         barbershopId,
